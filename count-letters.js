47e8abe0c98fc804a0c525b8fd8ebdce1d89239f
Justin Lum <j.lum07@gmail.com>
2018-04-10 22:37:41 +0000

    added for loop to add letters and the number of times they have appeared into an object and sucessfully returned the results

--- count-letters.js
+++ count-letters.js
@@ -1,13 +1,15 @@
 function countLetters(inStr) {
 
+  var count = {};
   var str = inStr.replace(/\s/g,'');
   var arrayStr = str.split("");
-  console.log(arrayStr);
 
   for (var i = 0; i < arrayStr.length; i++) {
-
+    count[arrayStr[i]] ? count[arrayStr[i]]++ : count[arrayStr[i]] = 1;
   }
 
+  return count;
 }
 
-countLetters("lighthouse in the house");
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("the quick brown fox jumped over the lazy dog"));
