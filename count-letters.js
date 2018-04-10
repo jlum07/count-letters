4670483dbe884feccfdcd828f3d3570e21ee919e
Justin Lum <j.lum07@gmail.com>
2018-04-10 22:26:51 +0000

    removed spaces from input string and split into an array

---
+++ count-letters.js
@@ -0,0 +1,13 @@
+function countLetters(inStr) {
+
+  var str = inStr.replace(/\s/g,'');
+  var arrayStr = str.split("");
+  console.log(arrayStr);
+
+  for (var i = 0; i < arrayStr.length; i++) {
+
+  }
+
+}
+
+countLetters("lighthouse in the house");
